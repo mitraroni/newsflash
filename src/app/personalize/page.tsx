48349b4c41790f45bf2f70698b0a4a6745617b4b
Xenturalt Codes It Solution <mitraroni15@gmail.com>
2025-10-26 06:32:31 +0000

    The skip button is not working

--- src/app/personalize/page.tsx
+++ src/app/personalize/page.tsx
@@ -71,7 +71,7 @@ export default function PersonalizeFeed() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <div className="flex justify-end p-4">
-        <Link href="/home">
+        <Link href="/home" passHref>
           <Button variant="ghost">Skip</Button>
         </Link>
       </div>
