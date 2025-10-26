6a00fe90acde07d172a4f8c697b9d32847a7323e
Xenturalt Codes It Solution <mitraroni15@gmail.com>
2025-10-26 06:19:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5075944878-e38eb",
+  "appId": "1:723326572705:web:41e7f2604a6b076e6578e0",
+  "apiKey": "AIzaSyB95otCZPEBcozgSw7JnGoqlKPZgKdrSl4",
+  "authDomain": "studio-5075944878-e38eb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "723326572705"
+};
