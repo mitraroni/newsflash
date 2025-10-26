a02306f4ae8a857fa0b6dd4761966c61bac94491
Xenturalt Codes It Solution <mitraroni15@gmail.com>
2025-10-26 05:26:11 +0000

    I am trying to click on search 🔎 but it's not working

--- src/components/footer-nav.tsx
+++ src/components/footer-nav.tsx
@@ -1,21 +1,43 @@
+
+'use client';
 import { Home, Search } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import placeholderImages from '@/lib/placeholder-images.json';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 export function FooterNav() {
   const newsUpdateLogo = placeholderImages.newsUpdateLogo;
+  const pathname = usePathname();
 
   return (
     <footer className="fixed bottom-0 left-0 right-0 bg-background border-t border-border/50 shadow-t-lg">
       <div className="container mx-auto px-4 h-16 flex items-center justify-around">
-        <Link href="/home/search" className="p-2">
-          <Search className="h-7 w-7 text-foreground" />
+        <Link
+          href="/home/search"
+          className={cn('p-2', {
+            'text-primary': pathname === '/home/search',
+            'text-foreground': pathname !== '/home/search',
+          })}
+        >
+          <Search className="h-7 w-7" />
         </Link>
-        <Link href="/home" className="p-2">
-          <Home className="h-7 w-7 text-foreground" />
+        <Link
+          href="/home"
+          className={cn('p-2', {
+            'text-primary': pathname === '/home',
+            'text-foreground': pathname !== '/home',
+          })}
+        >
+          <Home className="h-7 w-7" />
         </Link>
-        <Link href="/home/news-update" className="p-2">
+        <Link
+          href="/home/news-update"
+          className={cn('p-2', {
+            'border-primary border-2 rounded-full': pathname === '/home/news-update',
+          })}
+        >
           <Image
             src={newsUpdateLogo.src}
             alt={newsUpdateLogo.alt}
