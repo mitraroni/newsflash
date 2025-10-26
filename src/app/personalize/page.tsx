82bfb1596ae1dca2f037f0772ceb88a1e3fc0e12
Xenturalt Codes It Solution <mitraroni15@gmail.com>
2025-10-26 06:48:46 +0000

    Remove the skip button now

--- src/app/personalize/page.tsx
+++ src/app/personalize/page.tsx
@@ -11,7 +11,6 @@ import {
   type CarouselApi,
 } from '@/components/ui/carousel';
 import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
 
 const GoogleIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -23,7 +22,7 @@ const GoogleIcon = () => (
 );
 
 const FacebookIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0.0 24 24" fill="none">
     <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.099 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047v-2.66c0-3.025 1.791-4.697 4.533-4.697 1.312 0 2.688.235 2.688.235v2.953H15.83c-1.491 0-1.956.925-1.956 2.164v2.219h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.099 24 12.073Z" fill="white"/>
   </svg>
 );
@@ -71,13 +70,7 @@ export default function PersonalizeFeed() {
 
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <div className="flex justify-end p-4">
-        <Link href="/home" className={cn(buttonVariants({ variant: "ghost" }))}>
-          Skip
-        </Link>
-      </div>
-
-      <div className="flex flex-1 flex-col items-center justify-center text-center px-4">
+      <div className="flex-1 flex flex-col items-center justify-center text-center px-4 pt-10">
         <Carousel setApi={setApi} className="w-full max-w-xs">
           <CarouselContent>
             {slides.map((slide, index) => (
